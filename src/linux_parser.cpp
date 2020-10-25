69ee20f6e23554e693c323f6f60430a5e36e9235
William Kerstiens <tony.kerstiens@gmail.com>
2020-10-25 14:05:16 -0400

    Configuring CMake to use c++17

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -59,20 +59,6 @@ vector<int> LinuxParser::Pids() {
       }
     }
   }
-  // DIR* directory = opendir(kProcDirectory.c_str());
-  // struct dirent* file;
-  // while ((file = readdir(directory)) != nullptr) {
-  //   // Is this a directory?
-  //   if (file->d_type == DT_DIR) {
-  //     // Is every character of the name a digit?
-  //     string filename(file->d_name);
-  //     if (std::all_of(filename.begin(), filename.end(), isdigit)) {
-  //       int pid = stoi(filename);
-  //       pids.push_back(pid);
-  //     }
-  //   }
-  // }
-  // closedir(directory);
   return pids;
 }
 
